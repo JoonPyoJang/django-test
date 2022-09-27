c0ce9d75085467e2391922151a93f737f5d8ad8d
JoonPyoJang <jjpyo1009@gmail.com>
2022-09-27 10:33:33 +0900

    make Datatable creattime, location

--- introduce/models.py
+++ introduce/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class AccessLog(models.Model):
+    class Meta:
+        db_table = "logdata"
+    
+    created_at = models.DateTimeField("접속시간",auto_now_add = True)
+    location = models.CharField("접속경로",max_length=256, default='')
+
+    def __str__(self) -> str:
+        return f"{self.created_at}/{self.location}"
--- introduce/views.py
+++ introduce/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import AccessLog
 
 def test_introduce(request):
+    if request.method == 'GET':
+        logdata = AccessLog()
+        logdata.location = "introduce page 접속"
+        logdata.save()
+        
+    
     return render(request, 'main.html')
 # Create your views here.
